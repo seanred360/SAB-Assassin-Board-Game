229f136006bf833b9acbed09069945b20568d211
seanred360 <41050673+seanred360@users.noreply.github.com>
2019-05-05 13:06:16 +0800

    Initial commit

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+
+    public Sprite[] diceSides;
+    private Image m_rend;
+    private bool coroutineAllowed = true;
+    protected int m_randomDiceSide;
+    public static int DiceNumberThrown;
+
+    PlayerMover m_playerMover;
+    PlayerInput m_playerInput;
+
+    // Use this for initialization
+    private void Start () {
+        m_playerMover = FindObjectOfType<PlayerMover>();
+        m_playerInput = FindObjectOfType<PlayerInput>();
+        m_rend = GetComponent<Image>();
+        m_rend.sprite = diceSides[1];
+    }
+
+    public void StartRoll()
+    {
+        StartCoroutine("RollTheDice");
+    }
+
+    private IEnumerator RollTheDice()
+    {
+        for (int i = 0; i <= 20; i++)
+        {
+            //this.gameObject.GetComponent<Button>().interactable = false;
+            m_randomDiceSide = Random.Range(0, 2);
+            m_rend.sprite = diceSides[m_randomDiceSide];
+            yield return new WaitForSeconds(0.05f);
+        }
+        DiceNumberThrown = m_randomDiceSide + 1;
+        m_playerInput.SetMaxKeystrokes(m_randomDiceSide +1);
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // store horizontal input
+    float m_h;
+    public float H { get { return m_h; } }
+
+    // store the vertical input 
+    float m_v;
+    public float V { get { return m_v; } }
+
+    // global flag for enabling and disabling user input
+    bool m_inputEnabled = false;
+    public bool InputEnabled { get { return m_inputEnabled; } set { m_inputEnabled = value; } }
+
+    int m_maxKeystrokesAllowed;
+    public int MaxKeyStrokesAllowed { get { return m_maxKeystrokesAllowed; } set { m_maxKeystrokesAllowed = value; } }
+
+
+
+    void Update()
+    {
+        if (m_maxKeystrokesAllowed <= 0)
+        {
+            //m_inputEnabled = false;
+        }
+        else m_inputEnabled = true;
+    }
+
+    public void SetMaxKeystrokes(int diceNumberThrown)
+    {
+        m_maxKeystrokesAllowed = diceNumberThrown;
+    }
+
+    // get keyboard input
+    public void GetKeyInput()
+    {
+        //// if input is enabled, just get the raw axis data from the Horizontal and Vertical virtual axes (defined in InputManager)
+        //if (m_inputEnabled)
+        //{
+        //    m_h = Input.GetAxisRaw("Horizontal");
+        //    m_v = Input.GetAxisRaw("Vertical");
+        //}
+        //// if input is disabled, ensure that extra key input does not cause unintended movement
+        //else
+        //{
+        //    m_h = 0f;
+        //    m_v = 0f;
+        //}
+    }
+
+    public void OnPointerDownHorizontal(int direction)
+    {
+        if (m_inputEnabled)
+            m_h = direction;
+    }
+    public void OnPointerUpHorizontal()
+    {
+        if (m_inputEnabled)
+            m_h = 0;
+    }
+    public void OnPointerDownVertical(int direction)
+    {
+        if (m_inputEnabled)
+            m_v = direction;
+    }
+    public void OnPointerUpVertical()
+    {
+        if (m_inputEnabled)
+            m_v = 0;
+    }
+
+}
